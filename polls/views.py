855affeeede8766564dfe3029db1d764d9b9d649
thianrry <lisek.thianrry@aluno.ifsp.edu.br>
2023-05-16 11:12:52 -0300

    inclui models question and choice

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+#view index
+def index(request):
+    return HttpResponse("Olá, seja bem vindo a enquete")
+
+def sobre(request):
+    return HttpResponse("Este é um app de enquete")
