06c5e8739ea1ac470d29741b3c2a012536bbe6a2
thianrry <lisek.thianrry@aluno.ifsp.edu.br>
2023-06-20 11:10:17 -0300

    Inclui página com as últimas perguntas.

--- polls/views.py
+++ polls/views.py
@@ -12,3 +12,8 @@ def sobre(request):
 def exibe_questao(request, question_id):
     questao = Question.objects.get(id=question_id)
     return HttpResponse(questao.question_text)
+
+def ultimas_perguntas(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/perguntas.html', context)
