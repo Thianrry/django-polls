5ece219e9fb250793c465af85a95e06a94e48f3a
Thianrry <lisek.thianrry@aluno.ifsp.edu.br>
2023-08-29 10:33:45 -0300

    CREATE - CRUD Básico.

--- polls/urls.py
+++ polls/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 #importar da pasta atual o arquivo views.py
 from polls import views
+from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('sobre/', views.sobre, name='sobre'),
-    path('pergunta/<int:question_id>', views.exibe_questao, name='exibe_questao'),
-    path('perguntas', views.ultimas_perguntas, name='ultimas_perguntas')
+path('', views.index, name='index'),
+path('sobre/', views.sobre, name='sobre'),
+path('pergunta/<int:question_id>', views.exibe_questao, name='exibe_questao'),
+path('perguntas', views.ultimas_perguntas, name='ultimas_perguntas'),
+path('perguntas/list', views.ultimas_perguntas, name='polls_list'),
+path('pergunta/add', views.QuestionCreateView.as_view(), name="poll_add"),
 ]
