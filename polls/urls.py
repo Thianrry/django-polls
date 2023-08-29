01422f3c219704f18d4bb44c7c56c7cd93145e2a
Thianrry <lisek.thianrry@aluno.ifsp.edu.br>
2023-08-29 10:48:20 -0300

    UPDATE - CRUD Básico.

--- polls/urls.py
+++ polls/urls.py
@@ -11,4 +11,8 @@ path('pergunta/<int:question_id>', views.exibe_questao, name='exibe_questao'),
 path('perguntas', views.ultimas_perguntas, name='ultimas_perguntas'),
 path('perguntas/list', views.ultimas_perguntas, name='polls_list'),
 path('pergunta/add', views.QuestionCreateView.as_view(), name="poll_add"),
+path('pergunta/<int:pk>/edit',
+views.QuestionUpdateView.as_view(),
+name="poll_edit"
+),
 ]
